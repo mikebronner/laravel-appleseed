4248be358fd5dfab87dee67eb276bedde759a31e
Mike Bronner <mike@genealabs.com>
2015-11-16 16:53:37 -0800

    Cleaned out erroneous empty matches that caused everyting to 404

--- app/Http/Middleware/FaviconInterceptor.php
+++ app/Http/Middleware/FaviconInterceptor.php
@@ -16,6 +16,7 @@ class FaviconInterceptor
         $matches = [];
         $file = pathinfo($request->getUri());
         preg_match_all('/(apple-touch-icon.*?\.png|favicon(?:.*?)\.(?:png|ico)|android-chrome.*?.png|manifest.json|safari-pinned-tab.svg|mstile.*?.png)/', $file['basename'], $matches);
+        $matches = array_filter($matches);
 
         if ((count($matches) > 1) && (! file_exists(public_path($file['basename'])))) {
             abort(404);
